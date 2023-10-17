6943e29d36e73d8e235d67d4a6bb832056cf358a
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-10-18 00:04:34 +0100

    firebase configuration

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -4,6 +4,46 @@ import { SigninOptions, SignupOptions } from "../containers";
 import { useModal } from "../hooks";
 import { GAP, PADDING, icon } from "../../constants";
 import { jobType } from "../types/type";
+import { FullButton } from "../components/button";
+
+const data: jobType[] = [
+  {
+    id: "1",
+    company: "google",
+    role: "frontend developer intern",
+    logo: icon.google,
+    mode: "remote",
+    time: "20",
+    location: "Lagos, Nigeria",
+  },
+  {
+    id: "2",
+    company: "bowal",
+    role: "product design intern",
+    logo: icon.google,
+    mode: "hybrid",
+    time: "5",
+    location: "Lagos, Nigeria",
+  },
+  {
+    id: "3",
+    company: "google",
+    role: "backend developer intern",
+    logo: icon.google,
+    mode: "onsite",
+    time: "2",
+    location: "Lagos, Nigeria",
+  },
+  {
+    id: "4",
+    company: "google",
+    role: "backend developer intern",
+    logo: icon.google,
+    mode: "onsite",
+    time: "2",
+    location: "Lagos, Nigeria",
+  },
+];
 
 const Home = () => {
   const {
@@ -15,45 +55,6 @@ const Home = () => {
     openModal,
   } = useModal();
 
-  const data: jobType[] = [
-    {
-      id: "1",
-      company: "google",
-      role: "frontend developer intern",
-      logo: icon.google,
-      mode: "remote",
-      time: "20",
-      location: "Lagos, Nigeria",
-    },
-    {
-      id: "2",
-      company: "bowal",
-      role: "product design intern",
-      logo: icon.google,
-      mode: "hybrid",
-      time: "5",
-      location: "Lagos, Nigeria",
-    },
-    {
-      id: "3",
-      company: "google",
-      role: "backend developer intern",
-      logo: icon.google,
-      mode: "onsite",
-      time: "2",
-      location: "Lagos, Nigeria",
-    },
-    {
-      id: "4",
-      company: "google",
-      role: "backend developer intern",
-      logo: icon.google,
-      mode: "onsite",
-      time: "2",
-      location: "Lagos, Nigeria",
-    },
-  ];
-
   const renderItem: ListRenderItem<jobType> = ({ item }) => {
     return <JobCard item={item} />;
   };
@@ -61,6 +62,18 @@ const Home = () => {
   return (
     <View style={styles.body}>
       <DormantSearchFrame />
+
+      <View
+        style={{
+          position: "absolute",
+          width: "100%",
+          bottom: 70,
+          zIndex: 100,
+          alignSelf: "center",
+        }}
+      >
+        <FullButton label="Sign in" onPress={openModal} />
+      </View>
 
       <FlatList
         data={data}
